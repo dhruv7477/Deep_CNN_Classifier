a9e3ff0b5e48e13b63ed72e3e65d461efe3ea0b4
Dhruv Sharma <dhruv7477@gmail.com>
2022-12-07 12:30:59 +0530

    Init Setup Done!

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START" 
-echo [$(date)]: "Creating env with python 3.8 version" 
-conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "Creating env with python 3.9 version" 
+conda create --prefix ./env python=3.9 -y
 echo [$(date)]: "Activating the environment" 
 source activate ./env
 echo [$(date)]: "Installing the dev requirements" 
